def27cfa940f75e41686f0ddc87c1ed1afaee8ec
Bill Zaumen <bill.zaumen@gmail.com>
2022-02-07 19:24:57 -0800

    Add a missing class (EjwsStateTable)

---
+++ src/org.bzdev.ejws/org/bzdev/ejws/EjwsStateTable.java
@@ -0,0 +1,52 @@
+package org.bzdev.ejws;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.bzdev.net.HttpSessionOps;
+
+
+public class EjwsStateTable implements HttpSessionOps {
+
+    Map<String,Object> map;
+
+    public EjwsStateTable() {
+	map = new HashMap();
+    }
+
+    public EjwsStateTable(int size) {
+	map = new HashMap(size);
+    }
+
+    @Override
+    public void remove(String sid) {
+	map.remove(sid);
+    }
+
+    @Override
+    public void put(String sid, Object state) {
+	map.put(sid, state);
+    }
+
+    @Override
+    public void rename(String oldID, String newID)
+	throws IllegalStateException
+    {
+	if (!map.containsKey(oldID) || map.containsKey(newID)) {
+	    throw new IllegalStateException();
+	}
+	Object state = map.remove(oldID);
+	map.put(newID, state);
+    }
+
+
+    @Override
+    public boolean contains(String sid) {
+	return map.containsKey(sid);
+    }
+
+    @Override
+    public Object get(String sid) {
+	return map.get(sid);
+    }
+
+}
